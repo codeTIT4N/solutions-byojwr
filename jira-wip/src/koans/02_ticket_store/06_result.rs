8d7522db2d0fc4a63f63ca0f09a4675c5151a859
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-10-19 22:42:03 +0530

    most solutions

--- jira-wip/src/koans/02_ticket_store/06_result.rs
+++ jira-wip/src/koans/02_ticket_store/06_result.rs
@@ -58,10 +58,10 @@ impl TicketDraft {
             return Err(ValidationError("Title cannot be empty!".to_string()));
         }
         if title.len() > 50 {
-            todo!()
+            return Err(ValidationError("Invalid title length!".to_string()));
         }
         if description.len() > 3000 {
-            todo!()
+            return Err(ValidationError("Invalid description length!".to_string()));
         }
 
         let draft = TicketDraft { title, description };
